b5e89903ebc8830cfff06148aec3ef52f0bf327a
Vitor Parisio <vitorparisio@yahoo.com.br>
2020-12-14 23:29:17 -0300

    autocomplete

--- components/clientform/index.js
+++ components/clientform/index.js
@@ -2,24 +2,24 @@ import styles from './style.module.css'
 
 export default function ClientForm(){
     return(
-        <div className={styles.clientform}>
+        <div className={styles.clientform} autoComplete="off">
             <label htmlFor="name">
-                <input name="name" placeholder="Nome: " className={styles.clientdata}/>
+                <input name="name" placeholder="Nome: " className={styles.clientdata} autoComplete="off"/>
             </label>
 			<label htmlFor="email">
-                <input name="email" placeholder="Email: " className={styles.clientdata}/>
+                <input name="email" placeholder="Email: " className={styles.clientdata}  autoComplete='off'/>
             </label>
 			<label htmlFor="address">
-                <input name="address" placeholder="Endereço: " className={styles.clientdata}/>
+                <input name="address" placeholder="Endereço: " className={styles.clientdata} autoComplete='nope'/>
             </label>
 			<label htmlFor="city">
-                <input name="city" placeholder="Cidade: " className={styles.clientdata}/>
+                <input name="city" placeholder="Cidade: " className={styles.clientdata} autoComplete='off'/>
             </label>
 			<label htmlFor="state">
-                <input name="state" placeholder="Estado: " className={styles.clientdata}/>
+                <input name="state" placeholder="Estado: " className={styles.clientdata} autoComplete='off'/>
             </label>
 			<label htmlFor="postal_code">
-                <input name="postal_code" placeholder="Código postal: " className={styles.clientdata}/>
+                <input name="postal_code" placeholder="Código postal: " className={styles.clientdata} autoComplete='off'/>
             </label>
         </div>
     )
